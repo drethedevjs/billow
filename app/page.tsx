ac437de372782adf2668ef1406140c7e4a132648
Andre Thomas <drethedevjs@gmail.com>
2025-04-25 09:25:07 -0400

    Added login text above login form.

--- app/page.tsx
+++ app/page.tsx
@@ -5,35 +5,40 @@ export default function Home() {
     <>
       <div className="w-full flex flex-col md:flex-row p-10">
         <div className="w-full md:w-1/2">
-          <form className="flex max-w-md flex-col gap-4 mx-auto">
-            <div>
-              <div className="mb-2 block">
-                <Label htmlFor="email1">Your email</Label>
+          <div className="mx-auto max-w-md">
+            <p className="!text-4xl mb-5 text-primary dark:text-accent font-semibold text-center">
+              Log In
+            </p>
+            <form className="flex flex-col gap-4">
+              <div>
+                <div className="mb-2 block">
+                  <Label htmlFor="email1">Your email</Label>
+                </div>
+                <TextInput
+                  id="email1"
+                  type="email"
+                  placeholder="name@billow.com"
+                  required
+                />
               </div>
-              <TextInput
-                id="email1"
-                type="email"
-                placeholder="name@billow.com"
-                required
-              />
-            </div>
-            <div>
-              <div className="mb-2 block">
-                <Label htmlFor="password1">Your password</Label>
+              <div>
+                <div className="mb-2 block">
+                  <Label htmlFor="password1">Your password</Label>
+                </div>
+                <TextInput id="password1" type="password" required />
               </div>
-              <TextInput id="password1" type="password" required />
-            </div>
-            <div className="flex items-center gap-2">
-              <Checkbox id="remember" />
-              <Label htmlFor="remember">Remember me</Label>
-            </div>
-            <Button
-              type="submit"
-              className="bg-primary hover:bg-accent transition-colors"
-            >
-              Submit
-            </Button>
-          </form>
+              <div className="flex items-center gap-2">
+                <Checkbox id="remember" />
+                <Label htmlFor="remember">Remember me</Label>
+              </div>
+              <Button
+                type="submit"
+                className="bg-primary hover:bg-accent transition-colors"
+              >
+                Submit
+              </Button>
+            </form>
+          </div>
         </div>
         <div className="w-full md:w-1/2">
           <h1 className="mb-4 text-primary">Welcome to Billow!</h1>
