fec847b285e07eb3bda5ec007e948bc1a32bfb99
Andre Thomas <drethedevjs@gmail.com>
2025-05-02 16:58:36 -0400

    Added payment function (simulator) to dashboard.

--- components/portal/Dashboard.tsx
+++ components/portal/Dashboard.tsx
@@ -2,19 +2,20 @@
 import { accountData as data } from "@/data/accountData";
 import { AccountData } from "@/interfaces/account/AccountData";
 import Location from "@/interfaces/account/Location";
+import Service from "@/interfaces/account/Service";
 import type { RootState } from "@/store/configureStore";
 import {
-  Button,
   Table,
   TableBody,
   TableCell,
   TableHead,
   TableHeadCell,
-  TableRow
+  TableRow,
+  TextInput
 } from "flowbite-react";
-import React, { useEffect, useState } from "react";
-import { BiDollarCircle } from "react-icons/bi";
+import React, { useEffect, useMemo, useState } from "react";
 import { useSelector } from "react-redux";
+import PaymentButton from "./PaymentButton";
 
 const Dashboard = () => {
   const whichAccount: number = 4;
@@ -26,6 +27,47 @@ const Dashboard = () => {
     data[whichAccount].locations[0]
   );
 
+  const [processing, setProcessing] = useState<boolean>(false);
+
+  const [servicePaymentAmounts, setServicePaymentAmounts] = useState<
+    Record<string, number>
+  >({});
+
+  const changeToMostPrice = (type: string, service: Service) => {
+    const element = document.getElementById(
+      type.toLowerCase()
+    ) as HTMLInputElement;
+
+    const mostCustomerCanPay = service.penalty + service.price;
+
+    if (element) {
+      element.value = mostCustomerCanPay.toString();
+    } else throw new Error(`Cannot find the id for the ${type} utility.`);
+
+    return mostCustomerCanPay;
+  };
+
+  const updateServicePaymentAmount = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    type: string
+  ): void => {
+    const amount = Number(e.target.value);
+
+    const service = location.services.find((s) => s.type === type);
+    if (!service) return;
+
+    if (service.price < amount) {
+      const mostCustomerCanPay = changeToMostPrice(type, service);
+      setServicePaymentAmounts((prev) => ({
+        ...prev,
+        [type]: mostCustomerCanPay
+      }));
+      return;
+    }
+
+    setServicePaymentAmounts((prev) => ({ ...prev, [type]: amount }));
+  };
+
   useEffect(() => {
     const firstAccount = document.getElementsByClassName("account-number")[0];
     firstAccount.classList.add("bg-accent");
@@ -33,6 +75,7 @@ const Dashboard = () => {
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const selectLocation = (e: any, accountNumber: number) => {
+    setServicePaymentAmounts({ "": 0 });
     const activeAccountEl = document.getElementsByClassName(
       "bg-accent account-number"
     );
@@ -45,14 +88,33 @@ const Dashboard = () => {
     setLocation(loc!);
   };
 
-  const getTotal = () => {
+  const processPayment = () => {
+    setProcessing(true);
+
+    setTimeout(() => {
+      setProcessing(false);
+    }, 3000);
+  };
+
+  const totalDue = useMemo(() => {
     return location.services
       .reduce(
         (acc, currentValue) => acc + currentValue.price + currentValue.penalty,
         0
       )
       .toFixed(2);
-  };
+  }, [location.services]);
+
+  const calcTotalPayment = useMemo(() => {
+    const paymentAmounts = Object.values(servicePaymentAmounts);
+
+    const total = paymentAmounts
+      .reduce((prev, current) => prev + current, 0)
+      .toFixed(2);
+
+    return Number(total);
+  }, [servicePaymentAmounts]);
+
   return (
     <>
       <h1>Dashboard</h1>
@@ -92,6 +154,7 @@ const Dashboard = () => {
                 <TableRow>
                   <TableHeadCell>Utility</TableHeadCell>
                   <TableHeadCell>Cost</TableHeadCell>
+                  <TableHeadCell>Payment</TableHeadCell>
                 </TableRow>
               </TableHead>
               <TableBody className="divide-y">
@@ -106,6 +169,20 @@ const Dashboard = () => {
                           {s.type}
                         </TableCell>
                         <TableCell>${s.price.toFixed(2)}</TableCell>
+                        <TableCell>
+                          <TextInput
+                            id={s.type.toLowerCase()}
+                            type="number"
+                            placeholder="0.00"
+                            min="1.00"
+                            step="0.01"
+                            max={s.price + s.penalty}
+                            addon="$"
+                            onChange={(e) =>
+                              updateServicePaymentAmount(e, s.type)
+                            }
+                          />
+                        </TableCell>
                       </TableRow>
                       {s.penalty > 0 ? (
                         <tr
@@ -118,6 +195,7 @@ const Dashboard = () => {
                           <td className="text-center">
                             ${s.penalty.toFixed(2)}
                           </td>
+                          <td></td>
                         </tr>
                       ) : null}
                     </React.Fragment>
@@ -128,17 +206,18 @@ const Dashboard = () => {
                   <TableCell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
                     Total Due:
                   </TableCell>
-                  <TableCell className="">${getTotal()}</TableCell>
+                  <TableCell className="text-primary">${totalDue}</TableCell>
+                  <TableCell className="text-accent">
+                    ${calcTotalPayment}
+                  </TableCell>
                 </TableRow>
               </TableBody>
             </Table>
-            <Button
-              href="/pay"
-              className="mt-3 w-full bg-primary hover:bg-accent transition-colors dark:bg-accent dark:hover:bg-slate dark:hover:text-accent"
-            >
-              <BiDollarCircle className="mr-2 h-5 w-5" />
-              Make Payment
-            </Button>
+            <PaymentButton
+              processing={processing}
+              processPayment={processPayment}
+              calcTotalPayment={calcTotalPayment}
+            />
           </div>
         </div>
       </div>
