e4a714622c4294bf97df71f3ac0777f6a5d00313
Andre Thomas <drethedevjs@gmail.com>
2025-05-07 11:31:54 -0400

    Updated footer.

--- components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center bg-primary">
-      this is the footer
-    </footer>
-  );
-};
-
-export default Footer;
---
+++ components/TheFooter.tsx
@@ -0,0 +1,30 @@
+import {
+  Footer,
+  FooterCopyright,
+  FooterLink,
+  FooterLinkGroup
+} from "flowbite-react";
+
+const TheFooter = () => {
+  return (
+    <Footer container>
+      <FooterCopyright href="/" by="Billow" year={new Date().getFullYear()} />
+      <span className="text-sm">
+        Powered By{" "}
+        <a
+          href="https://csra-web-services.vercel.app/"
+          target="_blank"
+          className="text-accent hover:text-error transition-colors"
+        >
+          CSRA Web Services
+        </a>
+      </span>
+      <FooterLinkGroup>
+        <FooterLink href="#">About</FooterLink>
+        <FooterLink href="#">Contact</FooterLink>
+      </FooterLinkGroup>
+    </Footer>
+  );
+};
+
+export default TheFooter;
