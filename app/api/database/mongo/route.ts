b1a23da73ebd00df1262eaaebf1c16e5f0b4210a
Andre Thomas <drethedevjs@gmail.com>
2025-09-09 22:21:07 -0400

    Removed commented code and redundancy.

--- app/api/database/mongo/route.ts
+++ app/api/database/mongo/route.ts
@@ -1,39 +1,21 @@
 import { User } from "@/interfaces/User";
 import { getMongoClient } from "@/utils/mongoHelper";
+import { NextRequest } from "next/server";
 
 const mongoClient = await getMongoClient();
 if (!mongoClient) throw new Error("Mongo client missing.");
 const db = mongoClient.db();
 
-export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
+export async function GET(request: NextRequest) {
+  const { searchParams } = request.nextUrl;
 
   if (searchParams.has("accessTokenByUserId")) {
-    console.log("THIS NEW WAY WORKS");
-    const userId = searchParams.get("accessTokenByUserId");
-    const collection = db.collection("Users");
-    const user: User | null = await collection.findOne<User>({ userId });
-
-    return Response.json({ accessToken: user?.accessToken });
-  } else if (searchParams.has("accessTokenByUserId")) {
-    console.log("THIS NEW WAY WORKS");
     const userId = searchParams.get("accessTokenByUserId");
     const collection = db.collection("Users");
     const user: User | null = await collection.findOne<User>({ userId });
 
     return Response.json({ accessToken: user?.accessToken });
   }
-  // const collection = db.collection("Users");
-  // const user: User | null = await collection.findOne<User>({ userId });
-
-  // return Response.json({ boom: "holla back" });
-  // const { searchParams } = new URL(request.url);
-  // const userId = searchParams.get("userId");
-
-  // const collection = db.collection("Users");
-  // const user: User | null = await collection.findOne<User>({ userId });
-
-  // return Response.json({ accessToken: user?.accessToken });
 }
 
 export async function POST(request: Request) {
