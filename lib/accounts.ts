aafe5cf83d142c21e3711c670c00f641c2370a37
Andre Thomas <drethedevjs@gmail.com>
2025-09-11 13:45:39 -0400

    Added errors to the old sqllite library.

--- lib/accounts.ts
+++ lib/accounts.ts
@@ -6,12 +6,19 @@ const db = sql("billow.db");
 export const storeAccountInformation = (
   request: StoreAccountInformationRequest
 ) => {
+  throw new Error(
+    "storeAccountInformation from the accounts.ts in lib is obsolete!"
+  );
   db.prepare(
     "INSERT INTO accounts (user_id, account_id, account_mask) VALUES (?, ?, ?)"
   ).run(request.userId, request.accountId, request.accountMask);
 };
 
 export const getAccountInformation = (userId: string) => {
+  throw new Error(
+    "getAccountInformation from the accounts.ts in lib is obsolete!"
+  );
+
   const row = db
     .prepare<[string], { account_id: string; account_mask: string }>(
       "SELECT account_id as accountId, account_mask as accountMask FROM accounts WHERE user_id = ?"
